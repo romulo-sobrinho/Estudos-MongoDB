929bfb7cdd0f2fd06c915f06515eae9b6ebae10c
Romulo <romulosilva001@gmail.com>
2022-08-24 10:01:30 -0300

    Atualização das rotas e template alllinks

--- routes/linkRoute.js
+++ routes/linkRoute.js
@@ -2,11 +2,11 @@ const express = require('express')
 const router = express.Router()
 const linkController = require('../controllers/linkController')
 
-router.get('/', (req, res) => res.render('index', {error: false, body: {}}))
-
-router.get('/all', linkController.showLinks)
+router.get('/', linkController.showLinks)
 
 router.get('/:title', linkController.redirect)
+
+router.get('/add', (req, res) => res.render('index', {error: false, body: {}}))
 
 router.post('/', linkController.addLink)
 
